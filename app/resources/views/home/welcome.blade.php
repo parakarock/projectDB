ba9e5d080aae247d0ab7a1946a53bfce72c09135
unknown <you@example.com>
2019-05-16 17:43:39 +0700

    test

--- app/resources/views/home/welcome.blade.php
+++ app/resources/views/home/welcome.blade.php
@@ -45,8 +45,6 @@
                             <div class="card-body row no-gutters align-items-center">
                                 <div class="col-auto">
                                     <!-- <i class="fas fa-search h4 text-body"></i> -->
-                                   
-
                                 </div>
                                 <!--end of col-->
                                 <div class="col">
